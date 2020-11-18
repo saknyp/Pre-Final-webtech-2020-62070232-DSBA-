b1049119dbcbadfc24831acfc03d43a75b9838e2
saknyp <73894449+saknyp@users.noreply.github.com>
2020-11-18 21:55:05 +0700

    Add files via upload

---
+++ 2/index.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $url = "https://dd-wtlab2020.netlify.app/pre-final/ezquiz.json";
+    $response = file_get_contents($url);
+    $result = json_decode($response);
+
+    foreach($result as $images){
+        foreach($images->url as $url){
+            echo $url;
+        }
+    }
+    ?>
+</body>
+</html>
